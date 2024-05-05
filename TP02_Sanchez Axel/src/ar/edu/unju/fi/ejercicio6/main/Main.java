0386a6664d246d047a6f40d2f176d1bcf67097cf
gabri <gabri@DESKTOP-D643OE0>
2024-05-05 19:47:56 -0300

    Ejercicio 6 resuelto, hecho en clase

--- TP02_Sanchez Axel/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ TP02_Sanchez Axel/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -1,10 +1,25 @@
 package ar.edu.unju.fi.ejercicio6.main;
 
+import ar.edu.unju.fi.ejercicio6.interfaces.funcionales.Converter;
+import ar.edu.unju.fi.ejercicio6.model.FelinoDomestico;
+import ar.edu.unju.fi.ejercicio6.model.FelinoSalvaje;
+
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Garfield
+		FelinoDomestico felino01 = new FelinoDomestico("Garfiel",(byte)12, 34);
+		Converter<FelinoDomestico, FelinoSalvaje> converter = x -> new FelinoSalvaje(x.getNombre(),x.getEdad(),x.getPeso());
+		FelinoSalvaje felino02 = converter.convert(felino01);
+		converter.mostrarObjeto(felino02);
+		
+		//Mufasa
+		FelinoSalvaje felino03 = new FelinoSalvaje("Mufasa",(byte)20, 54);
+		Converter<FelinoSalvaje, FelinoDomestico> converterNuevo = x -> new FelinoDomestico(x.getNombre(),x.getEdad(),x.getPeso());
+		FelinoDomestico felino04 = converterNuevo.convert(felino03);
+		converterNuevo.mostrarObjeto(felino04);
+		
 	}
-
+	
+	
 }
