c6c502bc0346df0189ae10f9d7080ce2b5876579
AxxelGS <gabriell.sanchez232@gmail.com>
2024-06-29 18:06:07 -0300

    Ejercicio 5 arreglado

--- TP02_Sanchez Axel/src/ar/edu/unju/fi/ejercicio5/main/Main.java
+++ TP02_Sanchez Axel/src/ar/edu/unju/fi/ejercicio5/main/Main.java
@@ -21,9 +21,9 @@ public class Main {
 			do {
 				valido=false;
 				System.out.print("\n*** MENU ***\n");
-				System.out.print("1- Mostrar Productos Disponibles\n");
-				System.out.print("2- Realizar una Compra\n");
-				System.out.print("3- Salir\n");
+				System.out.print("1) Mostrar Productos Disponibles\n");
+				System.out.print("2) Realizar una Compra\n");
+				System.out.print("3) Salir\n");
 				try {
 					System.out.print("Opcion: ");
 					option=Integer.parseInt(sc.next());
@@ -48,21 +48,21 @@ public class Main {
 	}
 	
 	private static void precargarProductos(List<Producto> productos) {
-		Producto p1=new Producto("01", "Microondas", 530f, Origenes.ARGENTINA, Categorias.ELECTRO_HOGAR, true);
-		Producto p2=new Producto("02", "Martillo", 20f, Origenes.BRASIL, Categorias.HERRAMIENTAS, false);
+		Producto p1=new Producto("01", "PC", 500f, Origenes.ARGENTINA, Categorias.ELECTRO_HOGAR, true);
+		Producto p2=new Producto("02", "Destornillador", 1500f, Origenes.BRASIL, Categorias.HERRAMIENTAS, false);
 		Producto p3=new Producto("03", "Microfono", 700f, Origenes.CHINA, Categorias.INFORMATICA, false);
 		Producto p4=new Producto("04", "Cable", 230f, Origenes.URUGUAY, Categorias.TELEFONIA, true);
-		Producto p5=new Producto("05", "TV", 120f, Origenes.ARGENTINA, Categorias.ELECTRO_HOGAR, false);
+		Producto p5=new Producto("05", "TV", 1510f, Origenes.ARGENTINA, Categorias.ELECTRO_HOGAR, false);
 		Producto p6=new Producto("06", "Clavo", 90f, Origenes.BRASIL, Categorias.HERRAMIENTAS, true);
 		Producto p7=new Producto("07", "Cable", 10f, Origenes.CHINA, Categorias.INFORMATICA, false);
 		Producto p8=new Producto("08", "Telefono", 5300f, Origenes.URUGUAY, Categorias.TELEFONIA, true);
 		Producto p9=new Producto("09", "Heladera", 5050f, Origenes.ARGENTINA, Categorias.ELECTRO_HOGAR, true);
-		Producto p10=new Producto("10", "Hacha", 900f, Origenes.BRASIL, Categorias.HERRAMIENTAS, false);
+		Producto p10=new Producto("10", "Oz", 900f, Origenes.BRASIL, Categorias.HERRAMIENTAS, false);
 		Producto p11=new Producto("11", "Mouse", 560f, Origenes.CHINA, Categorias.INFORMATICA, true);
 		Producto p12=new Producto("12", "Teclas", 190f, Origenes.URUGUAY, Categorias.TELEFONIA, false);
-		Producto p13=new Producto("13", "Calefactor", 299f, Origenes.ARGENTINA, Categorias.ELECTRO_HOGAR, true);
+		Producto p13=new Producto("13", "Ventilador", 299f, Origenes.ARGENTINA, Categorias.ELECTRO_HOGAR, true);
 		Producto p14=new Producto("14", "Soldador", 412f, Origenes.BRASIL, Categorias.HERRAMIENTAS, true);
-		Producto p15=new Producto("15", "Samsung", 397f, Origenes.CHINA, Categorias.INFORMATICA, false);
+		Producto p15=new Producto("15", "Iphone", 397f, Origenes.CHINA, Categorias.INFORMATICA, false);
 		productos.add(p1);
 		productos.add(p2);
 		productos.add(p3);
@@ -85,6 +85,9 @@ public class Main {
                 productosDisponibles.add(producto);
             }
         }
+        /*return productos.stream()
+				.filter(producto -> producto.isDisponible())        Alternativo
+                .collect(Collectors.toList());*/
 	}
 	private static void mostrarProductosDisponibles(List<Producto> productosDisponibles) {
 		System.out.println("\n***Productos Disponibles***\n");
@@ -107,20 +110,20 @@ public class Main {
 				monto=calcularMontoDelCarro(productosParaComprar);
 				System.out.print("\n*** MENU ***\n");
 				System.out.println("Monto Total: "+monto);
-				System.out.print("1- Agregar un producto\n");
-				System.out.print("2- Pago Efectivo -10% de descuento\n");
-				System.out.print("3- Pago con Tarjeta +15% de recarga\n");
-				System.out.print("4- Cancelar\n");
+				System.out.print("1) Agregar un producto\n");
+				System.out.print("2) Pago Efectivo (-10%)\n");
+				System.out.print("3) Pago con Tarjeta (+15%)\n");
+				System.out.print("4) Cancelar\n");
 				try {
 					System.out.print("Opcion: ");
 					option=Integer.parseInt(sc.next());
 					if(option<1||option>4) {
-						System.out.print("\nNo valido\n");
+						System.out.print("\nOpcion no valida\n");
 					}else {
 						valido=true;
 					}
 				}catch (NumberFormatException e) {
-					System.out.print("\nIngresar numero valido\n");
+					System.out.print("\nERROR: Ingrese un numero valido\n");
 				}
 			}while(valido==false);
 			switch(option) {
@@ -161,9 +164,9 @@ public class Main {
 			}
 		}
 		if(valido) {
-			System.out.println("\nProducto añadido\n");
+			System.out.println("\nProducto Agregado Al Carro\n");
 		}else {
-			System.out.println("\nProducto no Disponible\n");
+			System.out.println("\nProducto Inexistente o No Disponible\n");
 		}
 	}
 	private static void pagarConEfectivo(List<Producto> productosParaComprar, double monto) {
