dd63dc1eaf6f0068df2839ef069dcb1922384361
gabri <gabri@DESKTOP-D643OE0>
2024-06-29 14:30:47 -0300

    Ejercicio 1 arreglado

--- TP02_Sanchez Axel/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
+++ TP02_Sanchez Axel/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
@@ -1,45 +1,74 @@
 package ar.edu.unju.fi.ejercicio1.model;
+
 public class Producto {
-	public enum Origenes{
-		ARGENTINA,CHINA,BRASIL,URUGUAY;
-	}
-	public enum Categorias{
-		TELEFONIA,INFORMATICA,ELECTRO_HOGAR,HERRAMIENTAS;
-	}
-	private String codigo;
-	private String descripcion;
-	private float precioUnitario;
-	private Origenes origenFabricacion;
-	private Categorias categoria;
+	private String Codigo;
+	private String Descripcion;
+	private double PrecioU;
+	private OrigenFabricacion origenFabricacion;
+	private Categoria categoria;
+
 	
-	public String getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	public String getDescripcion() {
-		return descripcion;
-	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-	public float getPrecioUnitario() {
-		return precioUnitario;
-	}
-	public void setPrecioUnitario(float precioUnitario) {
-		this.precioUnitario = precioUnitario;
-	}
-	public Origenes getOrigenFabricacion() {
-		return origenFabricacion;
-	}
-	public void setOrigenFabricacion(Origenes origenFabricacion) {
+	public Producto(String codigo, String descripcion, double precioU, OrigenFabricacion origenFabricacion,
+			Categoria categoria) {
+		super();
+		Codigo = codigo;
+		Descripcion = descripcion;
+		PrecioU = precioU;
 		this.origenFabricacion = origenFabricacion;
-	}
-	public Categorias getCategoria() {
-		return categoria;
-	}
-	public void setCategoria(Categorias categoria) {
 		this.categoria = categoria;
 	}
+	
+	public Producto() {
+		
+	}
+	public String getCodigo() {
+		return Codigo;
+	}
+	public void setCodigo(String codigo) {
+		Codigo = codigo;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public double getPrecioU() {
+		return PrecioU;
+	}
+	public void setPrecioU(double precioU) {
+		PrecioU = precioU;
+	}
+	
+	public enum OrigenFabricacion{
+		ARGENTINA, CHINA, BRASIL, URUGUAY
+	}
+	
+	public enum Categoria{
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+	}
+
+	public OrigenFabricacion getOrigenFabricacion() {
+		return origenFabricacion;
+	}
+
+	public void setOrigenFabricacion(OrigenFabricacion origenFabricacion) {
+		this.origenFabricacion = origenFabricacion;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Codigo del producto es: " + Codigo + "\nDescripcion del producto: " + Descripcion + "\nPrecio del producto: " + PrecioU
+				+ "\nOrigen de fabricacion: " + origenFabricacion + "\nCategoria: " + categoria;
+	}
+	
+	
 }
